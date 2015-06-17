f4532c7418537a0e7399947e8d3754b92a007f41
asaquzzaman <asaquzzaman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-17 19:42:02 +0000

    add help tips
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-to-google-merchant-center/trunk@1182849 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/help.php
@@ -0,0 +1,20 @@
+<div class="woogoo-author-wrap">
+	<h3>If you fail to confugure google API, google merchant center and woocommerce
+		to google merchant center plugin then i will configure your system.</h3>
+	<strong>I need some information to configure woocommerce to google merchant center.
+		so if you are interest then you have to provide me the following infomation.</strong>
+	<br><ul class="woogoo-author-dis-ul">
+		<li>E-mail Address</li>
+		<li>E-mail Password</li>
+		<li><br></li>
+		<li>ftp URL</li>
+		<li>Username</li>
+		<li>Password</li>
+		<li><br></li>
+		<li>WP-admin (sites admin) URL</li>
+		<li>Username</li>
+		<li>Password</li>
+	</ul><br>
+	<h3>Please mail me <a href="mailto:joy.mishu@gmail.com" target="_top">joy.mishu@gmail.com</a></h3>
+	<strong>After configureation your private information will removed</strong>
+</div>
